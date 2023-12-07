dff94b25bf72edfa89c5e88a76916df980f8798e
DarxyDev <damongarvin@gmail.com>
2023-12-07 18:13:04 -0500

    minor updates to highlighting. need to modify how event listeners in this scene work

--- src/scripts/scenes/piece-placement.js
+++ src/scripts/scenes/piece-placement.js
@@ -7,11 +7,11 @@ const BOARD_WIDTH = gameState.get.game.boardWidth();
 const BOARD_HEIGHT = gameState.get.game.boardHeight();
 
 const ref = {
-    p1: { gameTiles:undefined, placedPieces: [] },
-    p2: { gameTiles:undefined, placedPieces: [] }
+    p1: { gameTiles: undefined, placedPieces: [] },
+    p2: { gameTiles: undefined, placedPieces: [] }
 };
 
-for (let i = 0; i < PIECE_COUNT; i++){
+for (let i = 0; i < PIECE_COUNT; i++) {
     ref.p1.placedPieces.push(false);
     ref.p2.placedPieces.push(false);
 }
@@ -29,14 +29,16 @@ function initPiecePlacement() {
         const gameBox = scene.querySelector('[pPlacementID="gameBox"]');
         const gameTiles = generateGameTiles(gameBox);
         gameTiles.forEach((tile) => {
-            tile.setAttribute('playerRef',playerRef);
+            tile.setAttribute('playerRef', playerRef);
             tile.addEventListener('mouseover', (e) => {
-                const posX = +tile.getAttribute('posX');
-                const posY = +tile.getAttribute('posY');
                 tile.classList.add('tile-hover');
                 _highlightPossibleTiles(tile);
-            })
-            tile.addEventListener('mouseleave', (e) => {
+            });
+            tile.addEventListener('mouseleave', (e) => { //might combine mouseleave into function to use onclick as well.
+                tile.classList.remove('tile-hover');
+                _removeHighlight(tile);
+            });
+            tile.addEventListener('onclick', (e) => {
                 tile.classList.remove('tile-hover');
                 _removeHighlight(tile);
             })
@@ -45,27 +47,32 @@ function initPiecePlacement() {
         return scene;
     }
 }
+
 export default initPiecePlacement
+//////////
 let activeTiles = [];
 function _highlightPossibleTiles(tile) {
-    const _checkFunctions = [_checkUp,_checkDown,_checkLeft,_checkRight];
+    const _checkFunctions = [_checkUp, _checkDown, _checkLeft, _checkRight];
     const posX = +tile.getAttribute('posX');
     const posY = +tile.getAttribute('posY');
     const index = (posY * BOARD_WIDTH) + posX;
     const playerRef = tile.getAttribute('playerRef');
     let possibleMove = true;
+
+    tile.classList.add('tile-greenbg'); //todo: change to red when all pieces placed
+    activeTiles.push(tile);             //  except when removing piece. 
     for (let i = 1; i <= PIECE_COUNT; i++) {
         if (ref[playerRef].placedPieces[i - 1]) {
             console.log('mark red here?');
             continue;
         }
-        _checkFunctions.forEach((checkDirection) =>{
+        _checkFunctions.forEach((checkDirection) => {
             let tileIndex = checkDirection(i);
-            if(tileIndex)_markTile(tileIndex);
+            if (tileIndex) _markTile(tileIndex);
         })
     }
     //
-    function _markTile(tileIndex){
+    function _markTile(tileIndex) {
         if (tileIndex !== false) {
             let activeTile = ref[playerRef].gameTiles[tileIndex];
             activeTile.classList.add('tile-greenbg');
@@ -74,27 +81,27 @@ function _highlightPossibleTiles(tile) {
     }
     function _checkUp(distance) {
         let newIndex = index - (distance * BOARD_WIDTH);
-        if(newIndex < 0) return false;
+        if (newIndex < 0) return false;
         return newIndex;
     }
-    function _checkDown(distance){
+    function _checkDown(distance) {
         let newIndex = index + (distance * BOARD_WIDTH);
-        if(newIndex >= (BOARD_HEIGHT * BOARD_WIDTH)) return false;
+        if (newIndex >= (BOARD_HEIGHT * BOARD_WIDTH)) return false;
         return newIndex;
     }
-    function _checkLeft(distance){
+    function _checkLeft(distance) {
         let newX = posX - distance;
-        if(newX < 0) return false;
-        return (posY * BOARD_WIDTH) + newX; 
+        if (newX < 0) return false;
+        return (posY * BOARD_WIDTH) + newX;
     }
-    function _checkRight(distance){
+    function _checkRight(distance) {
         let newX = posX + distance;
-        if(newX >= BOARD_WIDTH) return false;
+        if (newX >= BOARD_WIDTH) return false;
         return (posY * BOARD_WIDTH) + newX;
     }
 }
-function _removeHighlight(){
-    activeTiles.forEach((tile)=>{
+function _removeHighlight() {
+    activeTiles.forEach((tile) => {
         tile.classList.remove('tile-greenbg');
         tile.classList.remove('tile-redbg');
     });
